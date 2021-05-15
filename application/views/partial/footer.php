8428960c085b060d452f09a38ca4f732d958fee2
K.A.N.Tharushka <70030748+nehangatharushka@users.noreply.github.com>
2021-05-15 23:28:35 +0530

    stable v1.0

---
+++ application/views/partial/footer.php
@@ -0,0 +1,10 @@
+		</div>
+	</div>
+
+	<div id="footer">
+		<div class="jumbotron push-spaces">
+			<strong><a href="https://softwaretroopers.com" target="_blank"><?php echo 'Copyright © 2021 Design by Software Troopers'; ?></a></strong>.
+		</div>
+	</div>
+</body>
+</html>
